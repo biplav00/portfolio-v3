0236ab3153bf5eadfabc13fda572b1afe450ecc9
Biplav Subedi <64011098+biplav00@users.noreply.github.com>
2026-02-27 21:28:32 +0545

    Add pre-commit hook with lint-staged to auto-fix lint issues

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
 ["src/content/posts/api-testing-best-practices.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fapi-testing-best-practices.mdx&astroContentModuleFlag=true")],
-["src/content/posts/my-testing-journey.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fmy-testing-journey.mdx&astroContentModuleFlag=true")],
-["src/content/posts/getting-started-with-test-automation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fgetting-started-with-test-automation.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/getting-started-with-test-automation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fgetting-started-with-test-automation.mdx&astroContentModuleFlag=true")],
+["src/content/posts/my-testing-journey.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fmy-testing-journey.mdx&astroContentModuleFlag=true")]]);
 		
